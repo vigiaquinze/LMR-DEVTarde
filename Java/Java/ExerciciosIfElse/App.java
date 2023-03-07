10efb76e207ba039ddff5d23cc4555996583a0ee
alberto12345678900 <albertomeloteco@gmail.com>
2023-03-07 13:25:27 -0300

    new

--- Java/Java/ExerciciosIfElse/App.java
+++ Java/Java/ExerciciosIfElse/App.java
@@ -5,11 +5,6 @@ public class App {
     public static void main(String[] args) {
         Exercicios objeto1 = new Exercicios();
         objeto1.exerc1();
-        objeto1.exerc2();
-        objeto1.exerc3();
-        objeto1.exerc4();
-        objeto1.exerc5();
-        objeto1.exerc6();
     }
 }
 
