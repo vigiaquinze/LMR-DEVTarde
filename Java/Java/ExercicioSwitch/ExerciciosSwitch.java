3407e884deb94f616f60c959843fc692d2838186
vigiaquinze <victorhugopagliuca84@gmail.com>
2023-03-14 14:29:18 -0300

    14/3 14h29

--- Java/Java/ExercicioSwitch/ExerciciosSwitch.java
+++ Java/Java/ExercicioSwitch/ExerciciosSwitch.java
@@ -37,4 +37,40 @@ public class ExerciciosSwitch {
         //3. mostrar o resultado para o usuario//
         System.out.println(letraEscolhida+ " é uma "+tipoLetra);
     }
+    public void exercicio2() {
+        //1. receber o mes digitado pelo usuario, em formato de numero//
+        System.out.println("Digite o número do mês que você quer saber:");
+        int mesNumero = scan.nextInt();
+        //2. escolher o resultado referente ao mes//
+        String mesNome = "";
+        switch (mesNumero){
+            case 1: mesNome="Janeiro";
+            break;
+            case 2: mesNome="Fevereiro";
+            break;
+            case 3:mesNome="Março";
+            break;
+            case 4: mesNome="Abril";
+            break;
+            case 5: mesNome="Maio";
+            break;
+            case 6: mesNome="Junho";
+            break;
+            case 7: mesNome="Julho";
+            break;
+            case 8: mesNome="Agosto";
+            break;
+            case 9: mesNome="Setembro";
+            break;
+            case 10: mesNome="Outubro";
+            break;
+            case 11: mesNome="Novembro";
+            break;
+            case 12: mesNome="Dezembro";
+            break;
+            default: mesNome="um mês que não existe";
+        }
+        //3. mostrar o resultado para o usuario//
+        System.out.println("O número "+mesNumero+" é referente a "+mesNome);
+    }
 }
