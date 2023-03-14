8038a627971d7ec24ce397730216a02be8ec4ef3
vigiaquinze <victorhugopagliuca84@gmail.com>
2023-03-14 16:03:19 -0300

    14/3 16h

--- Java/Java/ExemploArray/App.java
+++ Java/Java/ExemploArray/App.java
@@ -1,7 +1,7 @@
 package ExemploArray;
 
 public class App {
-    public static void main() {
+    public static void main(String[] args) {
         Vetores objeto1 = new Vetores();
         objeto1.exemplo1();
     }
