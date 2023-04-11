245096bbf0754cd6ca50aa03add9fd3f59aa766b
vigiaquinze <victorhugopagliuca84@gmail.com>
2023-04-11 15:19:52 -0300

    11.4 15h19

---
+++ Java/IteracaoFor/App.java
@@ -0,0 +1,8 @@
+package IteracaoFor;
+
+public class App {
+    public static void main(String[] args) {
+        ExemploFor objeto1 = new ExemploFor();
+        objeto1.exemplo1();
+    }
+}
---
+++ Java/IteracaoFor/ExemploFor.java
@@ -0,0 +1,10 @@
+package IteracaoFor;
+
+import java.util.Scanner;
+
+public class ExemploFor {
+    Scanner scan = new Scanner(System.in);
+    public void exemplo1() {
+        for(int i=0;i<10;i++){System.out.println("O número da iteração é "+i);}
+    }
+}
