8aa8580ce637ed2201f74bf53de7acd47ba5f1be
vigiaquinze <victorhugopagliuca84@gmail.com>
2023-04-18 16:57:43 -0300

    18.4 16h56

--- Java/IteracaoFor/ExerciciosIteracao/App.java
+++ Java/IteracaoFor/ExerciciosIteracao/App.java
@@ -3,6 +3,10 @@ package IteracaoFor.ExerciciosIteracao;
 public class App {
     public static void main(String[] args) {
         ExerciciosExtra obj = new ExerciciosExtra();
-        obj.exercicio4Extra();
+        obj.exercicio1();
+        obj.exercicio2();
+        obj.exercicio3();
+        obj.exercicio4();
+        obj.exercicio5();
     }
 }
