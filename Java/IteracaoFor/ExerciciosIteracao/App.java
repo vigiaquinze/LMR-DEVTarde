088afc2fbf0b2b9c4117932a96e268b32b5ecdf7
Victor Hugo Barbosa <victorhugopagliuca84@gmail.com>
2023-04-25 16:26:32 +0000

    pendrive salvador

--- Java/IteracaoFor/ExerciciosIteracao/App.java
+++ Java/IteracaoFor/ExerciciosIteracao/App.java
@@ -3,6 +3,14 @@ package IteracaoFor.ExerciciosIteracao;
 public class App {
     public static void main(String[] args) {
         ExerciciosExtra obj = new ExerciciosExtra();
+        obj.exercicio1();
+        obj.exercicio2();
+        obj.exercicio3();
+        obj.exercicio4();
+        obj.exercicio5();
+        obj.exercicio6();
         obj.exercicio7();
+        obj.exercicio8();
+        obj.exercicio9();
     }
 }
