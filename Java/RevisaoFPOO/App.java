9a338e7caef06f16a8ab8cc70eedd15ec74726c2
vigiaquinze <victorhugopagliuca84@gmail.com>
2023-07-25 17:04:05 -0300

    25.7 17h

---
+++ Java/RevisaoFPOO/App.java
@@ -0,0 +1,9 @@
+package RevisaoFPOO;
+
+import RevisaoFPOO.Cadastro.Animais;
+
+public class App {
+    public static void main(String[] args) {
+        Animais objeto1 = new Animais("médio", 6.75, "Diogo", "Xico", "Hamster");
+    }
+}
---
+++ Java/RevisaoFPOO/Cadastro/Animais.java
@@ -0,0 +1,54 @@
+package RevisaoFPOO.Cadastro;
+
+public class Animais {
+    //Atributos
+    String porte;
+    double peso;
+    String proprietario;
+    String nome;
+    String especie;
+    //métodos
+    //construtor cheio
+    public Animais(String porte, double peso, String proprietario, String nome, String especie) {
+        this.porte = porte;
+        this.peso = peso;
+        this.proprietario = proprietario;
+        this.nome = nome;
+        this.especie = especie;
+    }
+    //construtor vazio
+    public Animais() {
+    }
+    //gets and sets
+    public String getPorte() {
+        return porte;
+    }
+    public void setPorte(String porte) {
+        this.porte = porte;
+    }
+    public double getPeso() {
+        return peso;
+    }
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+    public String getProprietario() {
+        return proprietario;
+    }
+    public void setProprietario(String proprietario) {
+        this.proprietario = proprietario;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEspecie() {
+        return especie;
+    }
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+    
+}
