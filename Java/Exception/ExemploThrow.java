538ab3c4c6b8e7d3844146a2ca9eb5332e0ee44a
vigiaquinze <victorhugopagliuca84@gmail.com>
2023-08-08 16:56:20 -0300

    08.08 17h

--- Java/Exception/ExemploThrow.java
+++ Java/Exception/ExemploThrow.java
@@ -5,32 +5,43 @@ import javax.swing.JOptionPane;
 public class ExemploThrow {
     public static void main(String[] args) {
         String login = null;
+        String dataNascimento = null;
         boolean app = true;
         while (app) {
-            try {
-                JOptionPane.showMessageDialog(null, "Bem vindo ao SENAI.");
-                login = JOptionPane.showInputDialog("Digite seu login:");
-                String senha = JOptionPane.showInputDialog("Digite sua senha:");
+            JOptionPane.showMessageDialog(null, "Bem vindo ao SENAI.");
+            login = JOptionPane.showInputDialog("Digite seu login:");
+            dataNascimento = JOptionPane.showInputDialog("Digite sua data de nascimento (DD/MM/AA):");
+            dataNascimento = dataNascimento.replace("/","");
+            boolean senhaErrada = true;
+            while (senhaErrada) {
+                try {
+                    String senha = JOptionPane.showInputDialog("Digite sua senha:");
                     if (senha.length() != 6) {
                         throw new Exception("Senha inválida! A senha deve conter exatamente 6 caracteres.");
+                    } else if (senha.equals(login)) {
+                        throw new Exception("Senha inválida! A senha não pode ser igual ao login.");
+                    } else if (senha.equals("123456")) {
+                        throw new Exception("Senha inválida! A senha não pode ser de números sequenciais.");
+                    } else if (senha.equals("012345")) {
+                        throw new Exception("Senha inválida! A senha não pode ser de números sequenciais.");
+                    } else if (senha.equals("")) {
+                        throw new Exception("Senha inválida! A senha não pode estar vazia.");
+                    } else if (senha.equals(dataNascimento)) {
+                        throw new Exception("Senha inválida! A senha não pode ser a sua data de nascimento.");
+                    } else if (senha.matches(".*[A-Z]+.*") == false){
+                        throw new Exception("A senha deve conter:\n* Pelo menos uma letra maiúscula\n* Pelo menos uma letra minúscula\n* Pelo menos um número");
+                    } else if (senha.matches(".*[a-z]+.*") == false){
+                        throw new Exception("A senha deve conter:\n* Pelo menos uma letra maiúscula\n* Pelo menos uma letra minúscula\n* Pelo menos um número");
+                    }  else if (senha.matches(".*[0-9]+.*") == false){
+                        throw new Exception("A senha deve conter:\n* Pelo menos uma letra maiúscula\n* Pelo menos uma letra minúscula\n* Pelo menos um número");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Seja bem-vindo, " + login);
+                        app = false;
+                        senhaErrada = false;
                     }
-                    else if (senha.equals(login)) {
-                        throw new Exception("A senha não pode ser igual ao login");
-                    }
-                    else if (senha.equals("123456")) {
-                        throw new Exception("Erro! A senha não pode ser de números sequenciais");
-                    }
-                    else if (senha.equals("012345")) {
-                        throw new Exception("Erro! A senha não pode ser de números sequenciais");
-                    }
-                    else if (senha.equals("")) {
-                        throw new Exception("Erro! A senha não pode estar vazia");
-                    }
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, e.getMessage());
-            } finally {
-                JOptionPane.showMessageDialog(null, "Seja bem-vindo, " + login);
-                app=false;
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, e.getMessage());
+                }
             }
         }
     }
