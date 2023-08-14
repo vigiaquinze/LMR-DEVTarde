86133b6bc51c41ae8505f7e267ddb9f414c66dbf
vigiaquinze <victorhugopagliuca84@gmail.com>
2023-08-14 15:17:11 -0300

    14.8 intervalo

--- PWBE/ProjetoPHP/index.php
+++ PWBE/ProjetoPHP/index.php
@@ -1,3 +1,40 @@
 <?php
-	echo "vhchefe em php";
+echo "vhchefe em php";
+echo "\n";
+?>
+<?php
+$a = "5";
+echo $a + 2;
+echo "\n";
+echo $a + '5';
+echo ' carros';
+echo "\n";
+?>
+<?php
+$numeroDado = mt_rand(1, 6);
+$numeroTexto = "";
+switch ($numeroDado) {
+	case 1:
+		$numeroTexto = "Um";
+		break;
+	case 2:
+		$numeroTexto = "Dois";
+		break;
+	case 3:
+		$numeroTexto = "Três";
+		break;
+	case 4:
+		$numeroTexto = "Quatro";
+		break;
+	case 5:
+		$numeroTexto = "Cinco";
+		break;
+	case 6:
+		$numeroTexto = "Seis";
+		break;
+}
+echo $numeroDado;
+echo " (";
+echo $numeroTexto;
+echo ")";
 ?>
