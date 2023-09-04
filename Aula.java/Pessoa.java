1fd548377dd0e9e01732ae5911e7b1cc6a5708be
vigiaquinze <victorhugopagliuca84@gmail.com>
2023-09-04 13:56:31 -0300

    04.09 13h56

---
+++ Aula.java/Pessoa.java
@@ -0,0 +1,42 @@
+package POOExercicios1;
+public class Pessoa {
+    private String nome;
+    private double altura;
+    private int diaNascimento;
+    private int mesNascimento;
+    private int anaNascimento;
+     public Pessoa (String nome, double altura, int diaNascimento, int mesNascimento, int anaNascimento) {
+        this.nome=nome;
+        this.altura=altura;
+        this.diaNascimento=diaNascimento;
+        this.mesNascimento=mesNascimento;
+        this.anaNascimento=anaNascimento;
+     }
+
+     public Pessoa(){
+
+     }
+
+     public String getNome(){
+        return nome;
+
+     }
+     public void setNome(String  nome) {
+        this.nome = nome;
+     }
+     public double getAltura() {
+        return altura;
+     }
+     public void setAltura(double altura) {
+        this.altura = altura;
+     }
+      public int getDiaNascimento() {
+        return diaNascimento;
+      }
+       public void setDia
+     
+
+
+}
+    
+}
