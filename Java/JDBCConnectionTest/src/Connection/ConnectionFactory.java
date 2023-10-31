773f90f57f5f2f2b6d9a66e646d451a7f8461044
ryangabriel27 <ryanjulio56@gmail.com>
2023-10-31 15:19:31 -0300

    31.10 15h20

---
+++ Java/JDBCConnectionTest/src/Connection/ConnectionFactory.java
@@ -0,0 +1,7 @@
+package Connection;
+
+public class ConnectionFactory {
+    private static final String URL = "jdbc:posgresql://localhost:5432/postgres";
+    private static final String USUARIO = "postgres";
+    private static final String SENHA = "postgres";
+}
