1dd80cf389fe32a0774e6fac59b3e1daa1426800
ryangabriel27 <ryanjulio56@gmail.com>
2023-10-31 15:23:29 -0300

    31.10 15h20

--- Java/JDBCConnectionTest/src/Connection/ConnectionFactory.java
+++ Java/JDBCConnectionTest/src/Connection/ConnectionFactory.java
@@ -1,9 +1,20 @@
 package Connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
     //atributos
     private static final String URL = "jdbc:posgresql://localhost:5432/postgres";
     private static final String USUARIO = "postgres";
     private static final String SENHA = "postgres";
     //métodos
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao obter conexão com o banco de dados");
+        }
+    }
 }
