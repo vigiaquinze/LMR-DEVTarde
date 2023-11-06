acb3b20efbbb19be3b74f0b4141e609667d0ce0f
ryangabriel27 <ryanjulio56@gmail.com>
2023-11-06 14:06:18 -0300

    06/11 14h06

--- Java/SALojaCarros/src/App.java
+++ Java/SALojaCarros/src/App.java
@@ -1,7 +1,5 @@
-import Connection.ConnectionDAO;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        new ConnectionDAO().apagarTabela();
+        System.out.println("Hello, World!");
     }
 }
